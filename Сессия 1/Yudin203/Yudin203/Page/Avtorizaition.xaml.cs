94b9309cd32a1da9a2622d2c23440ec2b4af74ff
gruppa203 <gruppa203git@mail.ru>
2023-12-08 15:51:28 +0300

    Initial commit

---
+++ Сессия 1/Yudin203/Yudin203/Page/Avtorizaition.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Yudin203.ApplicationData;
+
+namespace Yudin203.Page
+{
+    /// <summary>
+    /// Логика взаимодействия для Avtorizaition.xaml
+    /// </summary>
+    public partial class Avtorizaition : Page
+    {
+        public Avtorizaition()
+        {
+            InitializeComponent();
+
+        }
+        private void btnVhod_Click(object sender, RoutedEventArgs e)
+        {
+           try
+            {
+                var UserObj = Bd_YudinEntities2.GetContext().Users.FirstOrDefault(x => x.Login &&)
+                if (UserObj == null)                
+            }
+        }
+    }
+}
---
+++ Сессия 1/Yudin203/Yudin203/Page/Tovar.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Yudin203.ApplicationData;
+
+namespace Yudin203.Page
+{
+    /// <summary>
+    /// Логика взаимодействия для Tovar.xaml
+    /// </summary>
+    public partial class Tovar : Page
+    {
+        public Tovar()
+        {
+            InitializeComponent();
+            DGridTovar.ItemsSource = Bd_YudinEntities2.GetContext().Tovar.ToList();
+        }
+
+        private void DGridTovar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
